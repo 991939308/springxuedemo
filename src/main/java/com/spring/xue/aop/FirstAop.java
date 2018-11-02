9a570d5b718964af85850661f0fec9e3052b8307
xue123123 <991939308@qq.com>
2018-11-02 14:34:37 +0800

    firstcommit

---
+++ src/main/java/com/spring/xue/aop/FirstAop.java
@@ -0,0 +1,18 @@
+package com.spring.xue.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FirstAop {
+
+    /*
+     * @program: xue
+     * @create: 2018-11-02-13-27
+     *
+     */
+    @Pointcut("execution(public * *(..))")
+    public void beforaspectj(){}
+}
