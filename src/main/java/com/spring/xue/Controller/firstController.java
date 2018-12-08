2a77b688be81b98ceafd2296a4bd3dd382d79932
991939308 <991939308@qq.com>
2018-12-08 18:21:17 +0800

    添加POI 生成excel  poi下载

--- src/main/java/com/spring/xue/Controller/firstController.java
+++ src/main/java/com/spring/xue/Controller/firstController.java
@@ -1,6 +1,10 @@
 package com.spring.xue.Controller;
 
 import com.spring.xue.service.firstService;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ResourceUtils;
@@ -9,11 +13,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/")
@@ -42,4 +49,44 @@ public class firstController {
         obj.put("msg",200);
         return obj;
     }
+
+    @RequestMapping("/filedownload")
+    public String filedownload(HttpServletResponse response){
+//        File file = new File();//加载一个文件
+        List<String> list = Arrays.asList("1","2","3");
+        HSSFWorkbook workbook = new HSSFWorkbook();//一个excel文件
+        HSSFSheet sheet = workbook.createSheet("test");//新建一个work表
+        for(int i=0;i<list.size();i++){
+            HSSFRow row= sheet.createRow(i);
+            HSSFCell cell = row.createCell(i);
+            cell.setCellValue(list.get(i));
+        }
+
+
+        response.reset();
+        response.setHeader("content-type", "application/octet-stream");
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment;filename=" + "test.xls");
+
+//        ServletOutputStream sout = response.getOutputStream();
+//        BufferedOutputStream bouts = new BufferedOutputStream();
+//        byte[] buff = new byte[2048];
+
+        OutputStream out = null;
+        try {
+            out = response.getOutputStream();
+            workbook.write(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "200";
+    }
+
 }
