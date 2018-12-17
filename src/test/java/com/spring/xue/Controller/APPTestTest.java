e25d4981e1149450c8f19ba2d2abdb7a5d6c4124
991939308 <991939308@qq.com>
2018-12-17 20:53:00 +0800

    新建jsp分支

---
+++ src/test/java/com/spring/xue/Controller/APPTestTest.java
@@ -0,0 +1,14 @@
+package com.spring.xue.Controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class APPTestTest {
+
+    @Test
+    public void a() {
+
+        System.out.println("sa41212121");
+    }
+}
